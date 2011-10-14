67f50a85ac627cb3754c3c8bfcd2d0473003ccb4
cmader <christian.mader@univie.ac.at>
2011-10-14 16:24:54 +0200

    implemented openId authentification; not working yet

--- src/main/java/at/ait/dme/yuma4j/server/gui/YUMAWebSession.java
+++ src/main/java/at/ait/dme/yuma4j/server/gui/YUMAWebSession.java
@@ -1,28 +1,41 @@
 package at.ait.dme.yuma4j.server.gui;
 
+import org.apache.wicket.PageParameters;
 import org.apache.wicket.Request;
 import org.apache.wicket.authentication.AuthenticatedWebSession;
 import org.apache.wicket.authorization.strategies.role.Roles;
+import org.openid4java.association.AssociationException;
+import org.openid4java.consumer.ConsumerException;
+import org.openid4java.discovery.DiscoveryException;
+import org.openid4java.message.MessageException;
 
 import at.ait.dme.yuma4j.server.config.ServerConfig;
+import at.ait.dme.yuma4j.server.gui.admin.OpenIdAuthenticator;
 
 public class YUMAWebSession extends AuthenticatedWebSession {
 
 	private static final long serialVersionUID = -6703698595455093437L;
-
+	private boolean openIdAuthenticated;
+	
 	public YUMAWebSession(Request request) {
         super(request);
     }
-
+	
+	public void signInOpenId(OpenIdAuthenticator authenticator, PageParameters pageParameters) 
+		throws MessageException, DiscoveryException, AssociationException, ConsumerException
+	{
+		openIdAuthenticated = authenticator.authenticationSuccess(pageParameters);
+	}
+		
     @Override
     public boolean authenticate(final String username, final String password) {
     	ServerConfig config = ServerConfig.getInstance(WicketApplication.getConfig());
         return username.equals(config.getAdminUsername()) && password.equals(config.getAdminPassword());
     }
-
+    
     @Override
     public Roles getRoles() {
-        if (isSignedIn())
+        if (isSignedIn() || openIdAuthenticated)
             return new Roles(Roles.ADMIN);
 
         return null;
--- src/main/java/at/ait/dme/yuma4j/server/gui/admin/LoginPage.java
+++ src/main/java/at/ait/dme/yuma4j/server/gui/admin/LoginPage.java
@@ -1,7 +1,41 @@
 package at.ait.dme.yuma4j.server.gui.admin;
 
-import org.apache.wicket.authentication.pages.SignInPage;
+import org.apache.wicket.PageParameters;
+import org.apache.wicket.authentication.AuthenticatedWebSession;
+import org.apache.wicket.authentication.panel.SignInPanel;
+import org.apache.wicket.markup.html.WebPage;
+import org.openid4java.association.AssociationException;
+import org.openid4java.consumer.ConsumerException;
+import org.openid4java.discovery.DiscoveryException;
+import org.openid4java.message.MessageException;
 
-public class LoginPage extends SignInPage {
+import at.ait.dme.yuma4j.server.gui.YUMAWebSession;
 
+public class LoginPage extends WebPage {
+
+	private OpenIdAuthenticator openIdAuthenticator;
+	
+	public LoginPage() {
+		this(null);
+	}
+	
+	public LoginPage(PageParameters pageParams) {
+		openIdAuthenticator = new OpenIdAuthenticator();
+		
+		add(new SignInPanel("signInPanel"));
+		add(new OpenIdSignInPanel("openIdSignInPanel", openIdAuthenticator));
+		
+		if (pageParams != null && !pageParams.isEmpty()) {
+			String isReturn = pageParams.getString("is_return");
+		    if (isReturn != null && isReturn.equals("true")) {
+		    	try {
+					((YUMAWebSession) AuthenticatedWebSession.get()).signInOpenId(openIdAuthenticator, pageParams);
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} 
+			}
+		}
+	}
+	
 }

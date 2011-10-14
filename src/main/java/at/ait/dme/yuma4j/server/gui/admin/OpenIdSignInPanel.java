5ddb2fc482b37e070371667f11d37f5a14284668
cmader <christian.mader@univie.ac.at>
2011-10-14 16:27:43 +0200

    implemented openID authentification; not working yet

---
+++ src/main/java/at/ait/dme/yuma4j/server/gui/admin/OpenIdSignInPanel.java
@@ -0,0 +1,41 @@
+package at.ait.dme.yuma4j.server.gui.admin;
+
+import org.apache.wicket.markup.html.form.StatelessForm;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.Model;
+
+@SuppressWarnings("serial")
+class OpenIdSignInPanel extends Panel {
+
+	private OpenIdAuthenticator openIdAuthenticator;
+	private TextField<String> openId;
+	
+	private class OpenIdForm extends StatelessForm<Void>
+	{
+		public OpenIdForm(String id) {
+			super(id);
+			
+			openId = new TextField<String>("openid", new Model<String>("")); 
+			add(openId);
+		}
+
+		@Override
+		protected void onSubmit() {
+			try {
+				openIdAuthenticator.performAuthentication(openId.getInput());
+			} 
+			catch (Exception e) {
+				System.out.println("blabla");
+			}
+		}
+	}
+	
+	public OpenIdSignInPanel(String id, OpenIdAuthenticator openIdAuthenticator) {
+		super(id);
+		this.openIdAuthenticator = openIdAuthenticator;
+		
+		add(new OpenIdForm("openIdSignInForm"));
+	}
+	
+}
